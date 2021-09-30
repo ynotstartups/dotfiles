0587623dbc0d3d3cd9ead2705f8a80898ca81fbf
tiger.huang <tiger.huang@ly.st>
2021-09-30 15:35:20 +0100

    add install packages

--- README.md
+++
@@ -1,4 +0,0 @@
-Ubuntu Vim doesn't come with clipboard support
-----
-
-`sudo apt-get install vim-gtk3 -y`
---
+++ install_packages.sh
@@ -0,0 +1,17 @@
+# a window manager
+sudo apt install i3
+   
+# formats json string from standard input
+sudo apt install jq
+
+# chrome browser
+sudo apt install google-chrome-stable
+
+# Vim with clipboard support
+sudo apt install vim-gtks
+
+# ctags
+sudo apt install exuberant-ctags
+
+# media player and a terminal version
+sudo apt install vlc
