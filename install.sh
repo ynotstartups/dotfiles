3b909e9e8c084173f2a8b3f79b467b0b0319dbb2
tiger.huang <tiger.huang@ly.st>
2021-09-30 11:19:51 +0100

    fix i3 status config installation

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -sf $PWD/i3-config $XDG_CONFIG_HOME/i3/config
 
 echo soft linking i3status config
 mkdir -p $XDG_CONFIG_HOME/i3status
-ln -sf $PWD/i3-status $XDG_CONFIG_HOME/i3status/config
+ln -sf $PWD/i3status-config $XDG_CONFIG_HOME/i3status/config
 
 echo install vim configs
 ln -sf $PWD/.vimrc $HOME/.vimrc
