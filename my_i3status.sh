fac920a372c0c071cc0c911e85b6bbd35d456a8f
ynotstartups <ynotstartups@gmail.com>
2022-10-26 09:28:35 +0100

    format i3 status output

--- my_i3status.sh
+++ my_i3status.sh
@@ -4,7 +4,11 @@
 i3status | while :
 do
     read line
+
+    # when a field is empty, i3status output 'BATTERY 99% |  | 10/26 09:19'
+    # remove the unnecessary "|  |"
+    formatted_line=`echo $line | sed 's/ |//'`
+
     weather=`curl -s wttr.in/?format=3 | cut -d ":" -f 2`
-    # weather='test'
-    echo "$weather | $line" || exit 1
+    echo "$weather | $formatted_line" || exit 1
 done
