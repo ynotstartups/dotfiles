7f6fbd0c5b0e2218e7dfd4b22dfbe4512dae64eb
ynotstartups <ynotstartups@gmail.com>
2022-10-29 10:12:37 +0100

    handle one space

--- my_i3status.sh
+++ my_i3status.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # shell script to prepend i3status with more stuff
 
 i3status | while :
@@ -7,7 +7,7 @@ do
 
     # when a field is empty, i3status output 'BATTERY 99% |  | 10/26 09:19'
     # remove the unnecessary "|  |"
-    formatted_line=`echo $line | sed 's/ |//'`
+    formatted_line=`echo $line | sed 's/ |  | / | /' | sed 's/ | | / | /'`
 
     # sed to change +19 to 19 and tr to change to uppercase
     weather=`curl -s wttr.in/?format=%C%t | sed 's/\+/ /' | tr a-z A-Z`
