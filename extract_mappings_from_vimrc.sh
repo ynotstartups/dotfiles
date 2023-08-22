14499bd18808e8ce1dbaa07e4673fa5fa00ec4a6
ynotstartups <ynotstartups@gmail.com>
2023-08-22 22:14:22 +0100

    checkpoint

--- extract_mappings_from_vimrc.sh
+++ extract_mappings_from_vimrc.sh
@@ -1,35 +1,32 @@
 #!/bin/sh
-# 
 
-# 
-# grep:  remove comments or let ..
-# fgrep: gives me lines with mapping keywords
-# sed:   remove modifier tags e.g. remove "<expr>"
-# grep:  remove anything up to the mapping, e.g. remove "autocmd FileType markdown"
-# sort:  sort by mapping keyword, then mapping
-# awk:   final format
-# 
-cat ~/.vimrc |\
-  grep -e '^"' -e '^let' -v |\
-    fgrep --word-regexp -e 'nmap' -e 'nnoremap' -e 'vmap' -e 'inoremap' -e 'map' |\
-      sed -e 's/<buffer>//g' -e 's/<expr>//g' -e 's/<silent>//g' |\
-          grep -o '\b[a-z]*map.*' |\
-            sort -k 1,2 |\
-              awk '{
-                # $1 is the mapping keyword e.g. nnoremap
-                # $2 is the mapping e.g. <leader>a
-                printf("%-15s %-20s", $1, $2);
-                for(i=3; i<=NF; ++i) printf("%s ", $i); printf("\n"); # print from the 3rd arguments to the last
-              }'
+function parse_mappings_or_commands() {
+    while read line; do
+        if [[ $line == command* ]]; then
+            echo "$line" |\
+                sed -e 's/ -bang -nargs=?//g'
+        else
+            echo "$line" |\
+                fgrep --word-regexp -e 'nmap' -e 'nnoremap' -e 'vmap' -e 'inoremap' -e 'map' |\
+                    sed -e 's/<buffer>//g' -e 's/<expr>//g' -e 's/<silent>//g' |\
+                        grep -o '\b[a-z]*map.*'
+        fi
+    done;
+}
 
-cat ~/.vimrc |\
-  grep '^command' |\
-    sed -e 's/ -bang -nargs=?//g' |\
-      sed -e 's/command!/command/g' |\
-        sort -k 1,2 |\
-          awk '{
-            # $1 is the mapping keyword e.g. nnoremap
-            # $2 is the mapping e.g. <leader>a
-            printf("%-15s %-20s", $1, $2);
-            for(i=3; i<=NF; ++i) printf("%s ", $i); printf("\n"); # print from the 3rd arguments to the last
-          }'
+function sort_and_format_output() {
+    # $1 is the mapping keyword e.g. nnoremap
+    # $2 is the mapping e.g. <leader>a
+    sort -k 1,2b |\
+        awk '{
+            printf("%-15s %-24s", $1, $2);
+            printf("\033[33m") # color yellow
+            for(i=3; i<=NF; ++i) printf("%s ", $i);
+                printf("\033[0m") # reset
+                printf("\n"); # print from the 3rd arguments to the last
+            }'
+}
+
+grep -e '^"' -e '^let' -v |\
+    parse_mappings_or_commands |\
+        sort_and_format_output
