9716831946335cc2d78344f3be26c076c68ddfde
ynotstartups <ynotstartups@gmail.com>
2023-08-24 23:43:35 +0100

    better placed comments

--- extract_mappings_commands_from_vimrc.sh
+++ extract_mappings_commands_from_vimrc.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 # parse and output mappings or commands from vimrc
 
-# grep:  remove comments or let ..
-# fgrep: gives me lines with mapping keywords
-# sed:   remove modifier tags e.g. remove "<expr>"
-# grep:  remove anything up to the mapping, e.g. remove "autocmd FileType markdown"
 function parse_mappings() {
+    # remove comments or let ..
     grep -e '^"' -e '^let' -v |\
+        # gives me lines with mapping keywords
         fgrep --word-regexp -e 'nmap' -e 'nnoremap' -e 'vmap' -e 'inoremap' -e 'map' |\
+            # remove modifier tags e.g. remove "<expr>"
             sed -e 's/<buffer>//g' -e 's/<expr>//g' -e 's/<silent>//g' |\
+                # remove anything up to the mapping, e.g. remove "autocmd FileType markdown"
                 grep -o '\b[a-z]*map.*'
 }
 
-# grep:  find commands, commands always start with `command!`
-# sed:   remove '-bang -nargs=?' followed from command!
 function parse_commands() {
+    # find commands, commands always start with `command!`
     grep -e '^command' |\
+        # remove '-bang -nargs=?' followed from command!
         sed -e 's/ -bang -nargs=?//g'
 }
 
@@ -23,13 +23,13 @@ function sort_and_format_output() {
     # $1 is the mapping or command! keyword e.g. nnoremap
     # $2 is the actual mapping or command e.g. <leader>a
     sort -k 1,2b |\
+        # the third variable definition reads all the rest of remaining arguments from this line
         while read type command definition
         do
             printf "%-15s %-24s \033[33m %s \033[0m\n" "$type"  "$command"  "$definition"
         done
 }
 
-# cat .vimrc twice are much faster compared to using if statements 
 cat ~/.vimrc |\
     parse_mappings |\
         sort_and_format_output
