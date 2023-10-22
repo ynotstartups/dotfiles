566304a361b81bdb39789882891d725a1914bd8a
ynotstartups <ynotstartups@gmail.com>
2023-10-22 22:02:25 +0100

    update config.fish

--- config.fish
+++ config.fish
@@ -11,6 +11,33 @@ function fish_hybrid_key_bindings --description \
     fish_vi_key_bindings --no-erase
 end
 
+set PATH /opt/homebrew/bin /usr/local/bin /usr/sbin $PATH
+
+#########
+# Theme #
+#########
+
+set fish_color_normal F8F8F2 # the default color
+set fish_color_command F92672 # the color for commands
+set fish_color_quote E6DB74 # the color for quoted blocks of text
+set fish_color_redirection AE81FF # the color for IO redirections
+set fish_color_end F8F8F2 # the color for process separators like ';' and '&'
+set fish_color_error F8F8F2 --background=F92672 # the color used to highlight potential errors
+set fish_color_param A6E22E # the color for regular command parameters
+set fish_color_comment 75715E # the color used for code comments
+set fish_color_match F8F8F2 # the color used to highlight matching parenthesis
+set fish_color_search_match --background=49483E # the color used to highlight history search matches
+set fish_color_operator AE81FF # the color for parameter expansion operators like '*' and '~'
+set fish_color_escape 66D9EF # the color used to highlight character escapes like '\n' and '\x70'
+set fish_color_cwd 66D9EF # the color used for the current working directory in the default prompt
+
+# Additionally, the following variables are available to change the highlighting in the completion pager:
+set fish_pager_color_prefix F8F8F2 # the color of the prefix string, i.e. the string that is to be completed
+set fish_pager_color_completion 75715E # the color of the completion itself
+set fish_pager_color_description 49483E # the color of the completion description
+set fish_pager_color_progress F8F8F2 # the color of the progress bar at the bottom left corner
+set fish_pager_color_secondary F8F8F2 # the background color of the every second completion
+
 set -g fish_key_bindings fish_hybrid_key_bindings
 
 alias e='exit'
@@ -22,6 +49,15 @@ alias e='exit'
 alias g='git'
 alias gs='git status'
 
+function ,g_s_notes;
+    set directories personal-notes dotfiles notes docs
+
+    for directory in $directories
+        set_color cyan; echo "$directory" \n; set_color normal;
+        git -C "$HOME/Documents/$directory" status
+        echo ""
+    end
+end
 
 set PERSONAL_NOTES "$HOME/Documents/personal-notes/"
 set NOTES          "$HOME/Documents/notes/"
@@ -32,3 +68,17 @@ alias ,ef="cd $DOTFILES       && vim config.fish"
 alias ,ev="cd $DOTFILES       && vim .vimrc"
 alias ,ew="cd $PERSONAL_NOTES && vim work_notes.md"
 alias ,ez="cd $DOTFILES       && vim .zshrc"
+
+#########
+# MacOS #
+#########
+
+alias ,hardcopy='lpr -p -o EPIJ_Silt=1 -o Resolution=720x720dpi -o EPIJ_Qual=307'
+alias ,hardcopy_normal_quality='lpr -p -o EPIJ_Silt=0 -o Resolution=360x360dpi -o EPIJ_Qual=303'
+alias ,hardcopy_10_standup_template='\
+    lpr -p -o EPIJ_Silt=0 -o scaling=110 -o Resolution=360x360dpi -o EPIJ_Qual=303 \
+    -# 10 ~/Documents/personal-notes/standup_template.pdf'
+
+# Cups link: http://localhost:631/
+# logins with laptops's username and password
+# /private/etc/cups/ppd
