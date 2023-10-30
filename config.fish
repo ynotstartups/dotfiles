f85d8dd7dbf3952de275bc3c6d323d01595755d5
ynotstartups <ynotstartups@gmail.com>
2023-10-30 22:36:05 +0000

    needs global for it to work

--- config.fish
+++ config.fish
@@ -22,10 +22,10 @@ function fish_prompt --description 'Informative prompt'
     #Save the return status of the previous command
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
-    set -g __fish_git_prompt_show_informative_status yes
-    set -g __fish_git_prompt_showdirtystate yes
-    set -g __fish_git_prompt_showupstream auto
-    set -g __fish_git_prompt_showcolorhints yes
+    set --global __fish_git_prompt_show_informative_status yes
+    set --global __fish_git_prompt_showdirtystate yes
+    set --global __fish_git_prompt_showupstream auto
+    set --global __fish_git_prompt_showcolorhints yes
 
     if functions -q fish_is_root_user; and fish_is_root_user
         printf '%s@%s %s%s%s# ' $USER (prompt_hostname) (set -q fish_color_cwd_root
