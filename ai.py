02c95fa24603be19fff7cc58802bed7d5b3e0d53
ynotstartups <ynotstartups@gmail.com>
2025-11-17 10:04:17 +0000

    improve ai script

--- ai.py
+++ ai.py
@@ -1,46 +1,113 @@
 #!/usr/bin/env python3
+"""
+- debug run `ollama run qwen2.5-coder:7b`
+- support continue_conversation in web by dumping all context in the web and
+  ask next question
+"""
+import json
+import logging
 import os
+import pathlib
+import pickle
+import subprocess
+from datetime import datetime
 
 from dotenv import load_dotenv
 from openai import OpenAI
 
+SCRIPT_PATH = pathlib.Path(__file__).parent.resolve()
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename=SCRIPT_PATH / "logs" / "ai.log",
+    format="%(asctime)s:%(levelname)s:%(module)s:%(message)s",
+)
 load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-"""
-TODO ideas
 
-- store question and answer in a sqllite db
-"""
+SYSTEM_PROMPT = "You are a helpful assistant for python backend django developers on MacOS, working in a UK finance wealth management firm."
 
 
-def get_answer(question: str):
-    client = OpenAI(api_key=OPENAI_API_KEY)
-    stream = client.responses.create(
-        model="gpt-5-nano",
-        input=[
+def get_answer_from_web(question: str, debug: bool):
+    system_prompt_and_question = f"{SYSTEM_PROMPT} {question}"
+    subprocess.run(
+        [
+            "open",
+            f"https://chatgpt.com/?q={system_prompt_and_question}",
+        ],
+    )
+
+
+def get_answer_from_api(question: str, continue_conversation: bool, debug: bool):
+    if continue_conversation:
+        with open(SCRIPT_PATH / "ai_db.jsonl", "r") as f:
+            for line in f:
+                lastline = line
+        inputs = json.loads(lastline.strip())["inputs"]
+        inputs.append(
+            {
+                "role": "user",
+                "content": question,
+            },
+        )
+    else:
+        inputs = [
             {
                 "role": "system",
-                "content": "You are a helpful assistant for python backend django developers.",
+                "content": SYSTEM_PROMPT,
             },
             {
                 "role": "user",
-                "content": f"I am a python backend django senior developer using MacOS, {question}",
+                "content": question,
             },
-        ],
-        text={"verbosity":'low'},
-        reasoning={'effort': 'low',}
-        stream=True,
-    )
-    for event in stream:
-        if event.type == "response.output_text.delta":
-            print(event.delta, end="", flush=True)
+        ]
 
-    if event.type == "response.completed":
-        print()
-        print("Usage Summary")
-        print(f"input token {event.response.usage.input_tokens}")
-        print(f"output token {event.response.usage.output_tokens}")
-        print(event.response.usage)
+    # client = OpenAI(api_key=OPENAI_API_KEY)
+    client = OpenAI(
+        base_url="http://localhost:11434/v1",  # Local Ollama API
+        api_key="ollama"                       # Dummy key
+    )
+    if not debug:
+        completion = client.chat.completions.create(
+            # model="qwen2.5-coder:7b",
+            model="gpt-oss:20b",
+            messages=inputs,
+            verbosity="low",
+            reasoning_effort="none",
+            stream=True,
+            store=False,
+            stream_options={
+                "include_usage":True,
+            },
+        )
+    else:
+        with open(SCRIPT_PATH / "ai_output" / "test.pickle", "rb") as f:
+            completion = pickle.load(f)
+
+    answer = ""
+    for chunk in completion:
+        if chunk.choices:
+            delta_answer = chunk.choices[0].delta.content
+            print(delta_answer, end="")
+            answer += chunk.choices[0].delta.content
+
+    print()
+    print("============================")
+    print("Usage Summary")
+    print(f"input token {chunk.usage.prompt_tokens}")
+    print(f"output token {chunk.usage.completion_tokens}")
+    print(chunk.usage)
+
+
+    inputs.append({"role": "assistant", "content": answer})
+    with open(SCRIPT_PATH / "ai_db.jsonl", "a") as f:
+        data = {
+            "inputs": inputs,
+            "date_created": datetime.now().isoformat(),
+            "debug": debug,
+            "continue_conversation": continue_conversation,
+        }
+        json.dump(data, f)
+        f.write("\n")
 
 
 if __name__ == "__main__":
@@ -53,25 +120,29 @@ if __name__ == "__main__":
         nargs="+",
         help="question to ask AI",
     )
+    parser.add_argument(
+        "--continue_conversation",
+        "-c",
+        action="store_true",
+        help="continue to ask question on last conversation",
+    )
+    parser.add_argument(
+        "--web",
+        "-w",
+        action="store_true",
+        help="open chatgpt web for this question",
+    )
+    parser.add_argument(
+        "--debug",
+        "-d",
+        action="store_true",
+        help="to use a fake streaming response instead of calling open-ai",
+    )
     args = parser.parse_args()
 
     question = " ".join(args.question)
 
-    get_answer(args.question)
-
-# import pickle
-# with open('stream_response.pickle', 'rb') as f:
-#     events = pickle.load(f)
-
-# for event in events:
-#     # if event.type == 'response.output_text.delta':
-#     #     print(event.delta, end='', flush=True)
-
-#     if event.type == "response.completed":
-#         from pprint import pp
-#         print(event.__dict__)
-#         print("============================"
-#         print('Usage Summary')
-#         print(f'input token {event.response.usage.input_token}')
-#         print(f'output token {event.response.usage.output_tokens}')
-#         print(event.response.usage)
+    if args.web:
+        get_answer_from_web(question, args.debug)
+    else:
+        get_answer_from_api(question, args.continue_conversation, args.debug)
